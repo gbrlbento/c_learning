6f3bb3b43f594d65d54dcc15f4db95db575a9d28
Gabriel Bento <gabrielestudos500@gmail.com>
2026-08-15 15:11:59 -0300

    feat: add first C program

---
+++ introduction/first_c_program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    // This is my first C program. (double slash is used for single line comments)
+
+    /* 
+    This
+    is 
+    my 
+    first 
+    C 
+    program. 
+    (slash and asterisk are used for multi-line comments) 
+    */
+
+    printf("First C Program!\n");
+
+    return 0;
+}
